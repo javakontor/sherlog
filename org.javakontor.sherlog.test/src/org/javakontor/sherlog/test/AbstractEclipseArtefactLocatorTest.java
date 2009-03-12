a6a5270dc73bfd4bbcc81363f3d686b076d41d22
nilshartmann <nilshartmann@9a91de95-6c99-490a-8b15-72008fb6e750>
2009-03-12 21:16:50 +0000

    ~ classpath

--- org.javakontor.sherlog.test/src/org/javakontor/sherlog/test/AbstractEclipseArtefactLocatorTest.java
+++ org.javakontor.sherlog.test/src/org/javakontor/sherlog/test/AbstractEclipseArtefactLocatorTest.java
@@ -1,7 +1,72 @@
 package org.javakontor.sherlog.test;
 
+import java.io.File;
+
 import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+import org.springframework.osgi.test.platform.OsgiPlatform;
+import org.springframework.osgi.test.provisioning.ArtifactLocator;
 
 public abstract class AbstractEclipseArtefactLocatorTest extends AbstractConfigurableBundleCreatorTests {
+  
+  /** - */
+  private EclipseArtifactLocator _eclipseArtifactLocator;
+
+  protected OsgiPlatform createPlatform() {
+
+    File root = new File(getWorkspaceLocation(), "target.platform");
+
+    File[] targetPlatformLocations = new File[6];
+
+    targetPlatformLocations[0] = new File(root, "equinox-3.5/plugins");
+    targetPlatformLocations[1] = new File(root, "ebr");
+    targetPlatformLocations[2] = new File(root, "codehaus");
+    targetPlatformLocations[3] = new File(root, "test/ebr");
+    targetPlatformLocations[4] = new File(root, "test/spring-2.5.5");
+    targetPlatformLocations[5] = new File(root, "test/spring-dm-1.1.3");
+
+    File workspaceLocation =getWorkspaceLocation();
+
+    this._eclipseArtifactLocator = new EclipseArtifactLocator(workspaceLocation, targetPlatformLocations);
+
+    System.setProperty("osgi.dev", "bin");
+
+    return super.createPlatform();
+  }
+  
+  protected File getWorkspaceLocation() {
+    File userDir = new File(System.getProperty("user.dir"));
+    File workspaceDir = userDir.getParentFile();
+    return workspaceDir;
+  }
+
+  protected String getManifestLocation() {
+    // TODO Auto-generated method stub
+
+    System.err.println();
+    System.err.println();
+    System.err.println();
+    System.err.println();
+
+    System.err.println("**************************************");
+    System.err.println("ManifestLocation: ");
+    System.err.println(super.getManifestLocation());
+    System.err.println("**************************************");
+
+    System.err.println();
+    System.err.println();
+    System.err.println();
+    System.err.println();
+
+    return super.getManifestLocation();
+  }
+
+  protected ArtifactLocator getLocator() {
+    return this._eclipseArtifactLocator;
+  }
+
+  protected String getRootPath() {
+    return "file:bin/";
+  }
+
   
 }
