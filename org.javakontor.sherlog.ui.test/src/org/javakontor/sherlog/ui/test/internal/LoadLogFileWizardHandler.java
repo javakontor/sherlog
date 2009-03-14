4c28753258ed35752f12ec25075dfdddc48112eb
nilshartmann <nilshartmann@9a91de95-6c99-490a-8b15-72008fb6e750>
2009-03-14 08:04:53 +0000

    jemmy "test"

---
+++ org.javakontor.sherlog.ui.test/src/org/javakontor/sherlog/ui/test/internal/LoadLogFileWizardHandler.java
@@ -0,0 +1,45 @@
+package org.javakontor.sherlog.ui.test.internal;
+
+import org.netbeans.jemmy.operators.AbstractButtonOperator;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JDialogOperator;
+import org.netbeans.jemmy.operators.WindowOperator;
+
+public class LoadLogFileWizardHandler extends AbstractViewHandler {
+
+  private final WindowOperator            _loadLogFileWizardOperator;
+
+  private final JButtonOperator           _okButtonOperator;
+
+  private final JButtonOperator           _cancelButtonOperator;
+
+  private final LogFileChooserViewHandler _logFileChooserViewHandler;
+
+  public LoadLogFileWizardHandler(WindowOperator parentOperator) {
+    this._loadLogFileWizardOperator = new JDialogOperator(parentOperator, "Load log file");
+
+    // TODO NLS ???
+    this._okButtonOperator = new JButtonOperator(_loadLogFileWizardOperator,
+        wrapTracingComponentChooser(new AbstractButtonOperator.AbstractButtonByLabelFinder("OK")));
+    this._cancelButtonOperator = new JButtonOperator(_loadLogFileWizardOperator, "Cancel");
+
+    this._logFileChooserViewHandler = new LogFileChooserViewHandler(_loadLogFileWizardOperator);
+  }
+
+  public WindowOperator getLoadLogFileWizardOperator() {
+    return _loadLogFileWizardOperator;
+  }
+
+  public JButtonOperator getOkButtonOperator() {
+    return _okButtonOperator;
+  }
+
+  public JButtonOperator getCancelButtonOperator() {
+    return _cancelButtonOperator;
+  }
+
+  public LogFileChooserViewHandler getLogFileChooserViewHandler() {
+    return _logFileChooserViewHandler;
+  }
+
+}
---
+++ org.javakontor.sherlog.ui.test/src/org/javakontor/sherlog/ui/test/internal/TracingComponentChooser.java
@@ -0,0 +1,31 @@
+package org.javakontor.sherlog.ui.test.internal;
+
+import java.awt.Component;
+
+import org.netbeans.jemmy.ComponentChooser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TracingComponentChooser implements ComponentChooser {
+
+	private final ComponentChooser	_delegate;
+
+	protected final Logger							_log	= LoggerFactory.getLogger(getClass());
+
+	public TracingComponentChooser(ComponentChooser delegate) {
+		super();
+		_delegate = delegate;
+	}
+
+	public boolean checkComponent(Component comp) {
+
+		_log.debug(String.format("While waiting for %s\n     checking component with name '%s' of type '%s' (%s)",
+				_delegate.getDescription(), comp.getName(), comp.getClass().getName(), comp));
+		return _delegate.checkComponent(comp);
+	}
+
+	public String getDescription() {
+		return "Traced Aufrufe fuer " + _delegate.getDescription();
+	}
+
+}
