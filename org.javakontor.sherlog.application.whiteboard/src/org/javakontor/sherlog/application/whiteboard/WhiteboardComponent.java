e5115801fc6bf8877ea29d55402ef01e6a5d9be9
gerdwuetherich <gerdwuetherich@9a91de95-6c99-490a-8b15-72008fb6e750>
2009-05-08 16:31:22 +0000

    ~ ServiceManager removed (112.5.7 Binding Services)

--- org.javakontor.sherlog.application.whiteboard/src/org/javakontor/sherlog/application/whiteboard/WhiteboardComponent.java
+++ org.javakontor.sherlog.application.whiteboard/src/org/javakontor/sherlog/application/whiteboard/WhiteboardComponent.java
@@ -3,10 +3,6 @@ package org.javakontor.sherlog.application.whiteboard;
 import org.javakontor.sherlog.application.action.contrib.ActionContribution;
 import org.javakontor.sherlog.application.action.contrib.ActionContributionAdmin;
 import org.javakontor.sherlog.application.action.contrib.ActionGroupContribution;
-import org.javakontor.sherlog.application.action.set.ActionSetManager;
-import org.javakontor.sherlog.util.servicemanager.DefaultServiceManager;
-import org.javakontor.sherlog.util.servicemanager.ServiceManagerEvent;
-import org.javakontor.sherlog.util.servicemanager.ServiceManagerListener;
 
 /**
  * <p>
@@ -19,70 +15,20 @@ import org.javakontor.sherlog.util.servicemanager.ServiceManagerListener;
  */
 public class WhiteboardComponent {
 
-  /** the service manager for actions */
-  private DefaultServiceManager<ActionContribution>       _actionServiceManager              = null;
-
-  /** the service manager listener for actions */
-  private ServiceManagerListener<ActionContribution>      _actionServiceManagerListener      = null;
-
-  /** the service manager for action groups */
-  private DefaultServiceManager<ActionGroupContribution>  _actionGroupServiceManager         = null;
-
-  /** the service manager listener for action groups */
-  private ServiceManagerListener<ActionGroupContribution> _actionGroupServiceManagerListener = null;
-
   /** the action set manager */
-  private ActionContributionAdmin                         _actionContributionAdmin           = null;
+  private ActionContributionAdmin                         _admin           = null;
 
   /**
    * <p>
-   * Creates a new instance of type {@link WhiteboardComponent}.
-   * </p>
-   */
-  public WhiteboardComponent() {
-
-    // create the ServiceManager
-    _actionServiceManager = new DefaultServiceManager<ActionContribution>();
-    _actionGroupServiceManager = new DefaultServiceManager<ActionGroupContribution>();
-
-    // create the ServiceManagerListener
-    _actionServiceManagerListener = new ServiceManagerListener<ActionContribution>() {
-      public void serviceAdded(ServiceManagerEvent<ActionContribution> event) {
-        _actionContributionAdmin.addActionContribution(event.getService());
-      }
-
-      public void serviceRemoved(ServiceManagerEvent<ActionContribution> event) {
-        _actionContributionAdmin.removeActionContribution(event.getService());
-      }
-    };
-
-    // remove the ServiceManagerListener
-    _actionGroupServiceManagerListener = new ServiceManagerListener<ActionGroupContribution>() {
-      public void serviceRemoved(ServiceManagerEvent<ActionGroupContribution> event) {
-        _actionContributionAdmin.removeActionGroupContribution(event.getService());
-      }
-
-      public void serviceAdded(ServiceManagerEvent<ActionGroupContribution> event) {
-        _actionContributionAdmin.addActionGroupContribution(event.getService());
-      }
-    };
-  }
-
-  /**
-   * <p>
-   * Called if an {@link ActionSetManager} is added.
+   * Called if an {@link ActionContributionAdmin} is added.
    * </p>
    *
    * @param actionContributionAdmin
-   *          the added {@link ActionSetManager}
+   *          the added {@link ActionContributionAdmin}
    */
   public void addActionContributionAdmin(ActionContributionAdmin actionContributionAdmin) {
     // set the admin
-    _actionContributionAdmin = actionContributionAdmin;
-
-    // add the ServiceManagerListeners
-    _actionServiceManager.addServiceManagerListener(_actionServiceManagerListener);
-    _actionGroupServiceManager.addServiceManagerListener(_actionGroupServiceManagerListener);
+    _admin = actionContributionAdmin;
   }
 
   /**
@@ -92,12 +38,8 @@ public class WhiteboardComponent {
    * @param actionSetManager
    */
   public void removeActionContributionAdmin(ActionContributionAdmin actionContributionAdmin) {
-    // remove the ServiceManagerListener
-    _actionServiceManager.removeServiceManagerListener(_actionServiceManagerListener);
-    _actionGroupServiceManager.removeServiceManagerListener(_actionGroupServiceManagerListener);
-
-    // unset the manager
-    _actionContributionAdmin = null;
+    // unset the admin
+    _admin = null;
   }
 
   /**
@@ -109,7 +51,7 @@ public class WhiteboardComponent {
    *          the added {@link ActionContribution}
    */
   public void addActionContribution(ActionContribution action) {
-    _actionServiceManager.bindService(action);
+    _admin.addActionContribution(action);
   }
 
   /**
@@ -121,7 +63,7 @@ public class WhiteboardComponent {
    *          the removed {@link ActionContribution}
    */
   public void removeActionContribution(ActionContribution action) {
-    _actionServiceManager.unbindService(action);
+    _admin.removeActionContribution(action);
   }
 
   /**
@@ -133,7 +75,7 @@ public class WhiteboardComponent {
    *          the added {@link ActionGroupContribution}
    */
   public void addActionGroupContribution(ActionGroupContribution actionGroup) {
-    _actionGroupServiceManager.bindService(actionGroup);
+    _admin.addActionGroupContribution(actionGroup);
   }
 
   /**
@@ -145,6 +87,6 @@ public class WhiteboardComponent {
    *          the removed {@link ActionGroupContribution}
    */
   public void removeActionGroupContribution(ActionGroupContribution actionGroup) {
-    _actionGroupServiceManager.unbindService(actionGroup);
+    _admin.removeActionGroupContribution(actionGroup);
   }
 }
