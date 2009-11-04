4687841990f24d9eee81c20ca2d3ac456488d5fd
nilshartmann <nilshartmann@9a91de95-6c99-490a-8b15-72008fb6e750>
2009-11-04 19:40:06 +0000

    ~ migration to equinox 3.5 (by matthias luebken)

--- sherlog-mac-starter/sherlog.sh
+++ sherlog-mac-starter/sherlog.sh
@@ -2,5 +2,5 @@
 #a script to start Sherlog
 echo "Starting sherlog"
 JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6
-$JAVA_HOME/Home/bin/java -Xmx512m -jar ../target.platform/equinox-3.4/plugins/org.eclipse.osgi_3.4.0.v20080529-1200.jar -classpath $JAVA_HOME/Classes/classes.jar:$JAVA_HOME/Classes/ui.jar -console -configuration ./configuration -dev bin -clean
+$JAVA_HOME/Home/bin/java -Xmx512m -jar ../target.platform/equinox-3.5/plugins/org.eclipse.osgi_3.5.0.v20090429-1630.jar -classpath $JAVA_HOME/Classes/classes.jar:$JAVA_HOME/Classes/ui.jar -console -configuration ./configuration -dev bin -clean
 
