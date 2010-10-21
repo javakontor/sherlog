6912ca472fe998db240b411961901d94b3a33a00
nilshartmann <nilshartmann@9a91de95-6c99-490a-8b15-72008fb6e750>
2010-10-21 12:16:41 +0000

    ~ Layout contributed components

--- org.javakontor.sherlog.application/src/org/javakontor/sherlog/application/internal/ApplicationStatusBar.java
+++ org.javakontor.sherlog.application/src/org/javakontor/sherlog/application/internal/ApplicationStatusBar.java
@@ -1,12 +1,16 @@
 package org.javakontor.sherlog.application.internal;
 
+import java.awt.Dimension;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
@@ -42,10 +46,11 @@ public class ApplicationStatusBar extends JPanel {
   public void refreshStatusbarContent() {
     removeAll();
 
+    // make sure all components are one the right side
     add(Box.createHorizontalGlue());
 
     for (final ApplicationStatusBarContribution contribution : this._contributions) {
-      add(contribution.getComponent());
+      add(wrapComponent(contribution.getComponent()));
     }
 
     add(this._memoryPanel);
@@ -71,6 +76,44 @@ public class ApplicationStatusBar extends JPanel {
     this._contributions.remove(contribution);
 
     refreshStatusbarContent();
+  }
+
+  private JComponent wrapComponent(final JComponent component) {
+    return new StatusBarPartPanel(component);
+  }
+
+  /**
+   * Holds a contributed UI component for the statusbar including a separator on the right side
+   * 
+   */
+  class StatusBarPartPanel extends JPanel {
+    /**
+     * the default serialVersionUID
+     */
+    private static final long serialVersionUID = 1L;
+
+    public StatusBarPartPanel(final JComponent contributedComponent) {
+      setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+      add(contributedComponent);
+
+      // add separator
+      add(Box.createHorizontalStrut(5)); // fixed space between separator and component
+      add(new JSeparator(SwingConstants.VERTICAL));
+      add(Box.createHorizontalStrut(5)); // fixed space
+
+    }
+
+    // ~~~ Size should always be as small as possible
+
+    @Override
+    public Dimension getPreferredSize() {
+      return super.getMinimumSize();
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+      return super.getMinimumSize();
+    }
   }
 
 }
