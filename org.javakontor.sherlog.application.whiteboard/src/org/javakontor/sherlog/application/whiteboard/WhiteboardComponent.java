994e00c56b5d8f5522f82b01d5a7e558d94e8a12
gerdwuetherich <gerdwuetherich@9a91de95-6c99-490a-8b15-72008fb6e750>
2010-10-24 19:42:44 +0000

    ~ several minor improvements for the apache felix support

--- org.javakontor.sherlog.application.whiteboard/src/org/javakontor/sherlog/application/whiteboard/WhiteboardComponent.java
+++ org.javakontor.sherlog.application.whiteboard/src/org/javakontor/sherlog/application/whiteboard/WhiteboardComponent.java
@@ -6,87 +6,97 @@ import org.javakontor.sherlog.application.action.contrib.ActionGroupContribution
 
 /**
  * <p>
- * Implements a 'whiteboard component'. The whiteboard component is responsible for tracking and registering all
- * {@link ActionContribution ActionContributions} and {@link ActionGroupContribution ActionGroupContributions} that are
- * registered with the OSGi service registry.
+ * Implements a 'whiteboard component'. The whiteboard component is responsible
+ * for tracking and registering all {@link ActionContribution
+ * ActionContributions} and {@link ActionGroupContribution
+ * ActionGroupContributions} that are registered with the OSGi service registry.
  * </p>
- *
+ * 
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
 public class WhiteboardComponent {
 
-  /** the action set manager */
-  private ActionContributionAdmin _admin = null;
+	/** the action set manager */
+	private ActionContributionAdmin _admin = null;
 
-  /**
-   * <p>
-   * Called if an {@link ActionContributionAdmin} is added.
-   * </p>
-   *
-   * @param actionContributionAdmin
-   *          the added {@link ActionContributionAdmin}
-   */
-  public void addActionContributionAdmin(ActionContributionAdmin actionContributionAdmin) {
-    // set the admin
-    _admin = actionContributionAdmin;
-  }
+	/**
+	 * <p>
+	 * Called if an {@link ActionContributionAdmin} is added.
+	 * </p>
+	 * 
+	 * @param actionContributionAdmin
+	 *            the added {@link ActionContributionAdmin}
+	 */
+	public void addActionContributionAdmin(
+			ActionContributionAdmin actionContributionAdmin) {
+		// set the admin
+		_admin = actionContributionAdmin;
+	}
 
-  /**
-   * <p>
-   * </p>
-   *
-   * @param actionSetManager
-   */
-  public void removeActionContributionAdmin(ActionContributionAdmin actionContributionAdmin) {
-    // unset the admin
-    _admin = null;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @param actionSetManager
+	 */
+	public void removeActionContributionAdmin(
+			ActionContributionAdmin actionContributionAdmin) {
+		// unset the admin
+		_admin = null;
+	}
 
-  /**
-   * <p>
-   * Called if an {@link ActionContribution} is added.
-   * </p>
-   *
-   * @param action
-   *          the added {@link ActionContribution}
-   */
-  public void addActionContribution(ActionContribution action) {
-    _admin.addActionContribution(action);
-  }
+	/**
+	 * <p>
+	 * Called if an {@link ActionContribution} is added.
+	 * </p>
+	 * 
+	 * @param action
+	 *            the added {@link ActionContribution}
+	 */
+	public void addActionContribution(ActionContribution action) {
+		_admin.addActionContribution(action);
+	}
 
-  /**
-   * <p>
-   * Called if an {@link ActionContribution} is removed.
-   * </p>
-   *
-   * @param action
-   *          the removed {@link ActionContribution}
-   */
-  public void removeActionContribution(ActionContribution action) {
-    _admin.removeActionContribution(action);
-  }
+	/**
+	 * <p>
+	 * Called if an {@link ActionContribution} is removed.
+	 * </p>
+	 * 
+	 * @param action
+	 *            the removed {@link ActionContribution}
+	 */
+	public void removeActionContribution(ActionContribution action) {
+		if (_admin != null) {
+			_admin.removeActionContribution(action);
+		}
+	}
 
-  /**
-   * <p>
-   * Called if an {@link ActionGroupContribution} is added.
-   * </p>
-   *
-   * @param actionGroup
-   *          the added {@link ActionGroupContribution}
-   */
-  public void addActionGroupContribution(ActionGroupContribution actionGroup) {
-    _admin.addActionGroupContribution(actionGroup);
-  }
+	/**
+	 * <p>
+	 * Called if an {@link ActionGroupContribution} is added.
+	 * </p>
+	 * 
+	 * @param actionGroup
+	 *            the added {@link ActionGroupContribution}
+	 */
+	public void addActionGroupContribution(ActionGroupContribution actionGroup) {
+		if (_admin != null) {
+			_admin.addActionGroupContribution(actionGroup);
+		}
+	}
 
-  /**
-   * <p>
-   * Called if an {@link ActionGroupContribution} is removed.
-   * </p>
-   *
-   * @param actionGroup
-   *          the removed {@link ActionGroupContribution}
-   */
-  public void removeActionGroupContribution(ActionGroupContribution actionGroup) {
-    _admin.removeActionGroupContribution(actionGroup);
-  }
+	/**
+	 * <p>
+	 * Called if an {@link ActionGroupContribution} is removed.
+	 * </p>
+	 * 
+	 * @param actionGroup
+	 *            the removed {@link ActionGroupContribution}
+	 */
+	public void removeActionGroupContribution(
+			ActionGroupContribution actionGroup) {
+		if (_admin != null) {
+			_admin.removeActionGroupContribution(actionGroup);
+		}
+	}
 }
