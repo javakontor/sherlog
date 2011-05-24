c431a063299c794c03f345d55b1cb8401dca88e2
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-05-24 11:01:39 +0200

    layout

--- sherlog-mac-starter/sherlog.sh
+++ sherlog-mac-starter/sherlog.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #a script to start Sherlog
+
 echo "Starting sherlog"
 JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6
 $JAVA_HOME/Home/bin/java -Xmx512m -jar ../target.platform/equinox-3.5/plugins/org.eclipse.osgi_3.5.0.v20090429-1630.jar -classpath $JAVA_HOME/Classes/classes.jar:$JAVA_HOME/Classes/ui.jar -console -configuration ./configuration -dev bin -clean
