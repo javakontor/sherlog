85facaadde9f921710ff66003a08ad783ee6d67c
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-01-12 10:23:13 +0100

    ~ added 'dummy' PDE-Test

---
+++ org.javakontor.sherlog.test.pde/src/org/javakontor/sherlog/test/pde/MyTest.java
@@ -0,0 +1,13 @@
+package org.javakontor.sherlog.test.pde;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
